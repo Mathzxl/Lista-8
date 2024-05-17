47c9487ad600361223bb1a3c91193901d60200af
Mathzxl <162761845+Mathzxl@users.noreply.github.com>
2024-05-17 20:38:13 -0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1
+{
+    class Program
+    {
+        static int MenorValor(int v1, int v2, int v3, int v4)
+        {
+            int menorv = v1;
+            if (v1 > v2)
+                menorv = v2;
+            else if (v2 > v3)
+                menorv = v3;
+            else if (v3 > v4)
+                menorv = v4;
+            return menorv;
+
+        }
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4;
+            Console.WriteLine("Dê um valor qualquer");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dê um valor qualquer");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dê um valor qualquer");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dê um valor qualquer");
+            n4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O menor valor é: " + MenorValor(n1, n2, n3, n4));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    class Program
+    {
+        static double Hipotenusa(int A, int B)
+        {
+            double calchip = Math.Sqrt((double)Math.Pow(A,2) + (double)Math.Pow(B,2));
+            return calchip;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            double hip;
+            Console.WriteLine("Dê um valor pro lador do triângulo");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dê um valor pro lador do triângulo");
+            b = int.Parse(Console.ReadLine());
+            hip = Hipotenusa(a, b);
+            Console.WriteLine("O valor da sua Hipotenusa é: " + hip);
+            Console.ReadLine();
+        }
+    }
+}
